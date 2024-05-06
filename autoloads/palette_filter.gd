7367d40ed6dd54dc9641166757f6e7dd58cb9bc2
Leo Feng <thcheetah777@gmail.com>
2024-05-06 15:11:25 -0500

    🎨 palette filter

--- autoloads/palette_filter.gd
+++ autoloads/palette_filter.gd
@@ -1,11 +1,26 @@
-class_name PaletteFilter extends CanvasLayer
+extends CanvasLayer
 
 @export var default_palette: PaletteResource
 
-var palette: PaletteResource
+var palette: PaletteResource:
+	get: return palette
+	set(value):
+		palette = value
+
+func _enter_tree() -> void:
+	RoomManager.room_entered.connect(_on_room_entered)
 
 func _ready() -> void:
-	RoomManager.room_entered.connect(_on_room_entered)
+	update_palette()
+
+func update_palette():
+	RenderingServer.set_default_clear_color(palette.dark)
+
+	$ColorRect.material.set_shader_parameter("new_dark", palette.dark)
+	$ColorRect.material.set_shader_parameter("new_light", palette.light)
+	$ColorRect.material.set_shader_parameter("new_primary", palette.primary)
+	$ColorRect.material.set_shader_parameter("new_secondary", palette.secondary)
+	$ColorRect.material.set_shader_parameter("new_accent", palette.accent)
 
 func _on_room_entered(room: Room):
 	if room.palette:
