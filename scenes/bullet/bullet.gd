df3e4df2a9dccc6b6e1aeef5b59d053884ecfc91
Leo Feng <thcheetah777@gmail.com>
2024-05-09 12:23:16 -0500

    🚅 buulet

---
+++ scenes/bullet/bullet.gd
@@ -0,0 +1,10 @@
+class_name Bullet extends Area2D
+
+var speed = 0.0
+
+func _ready() -> void:
+	if speed == 0.0:
+		printerr("🚅 \"" + name + "\" bullet speed is not specified! Bullet will not move.")
+
+func _physics_process(delta):
+	position += Vector2.from_angle(rotation) * speed * delta
