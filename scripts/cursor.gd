1833d57b3b13ae474cd3e02a97bf36dee3d4820c
Leo Feng <thcheetah777@gmail.com>
2024-05-09 11:44:28 -0500

    🐭 mouse cursor

---
+++ scripts/cursor.gd
@@ -0,0 +1,7 @@
+class_name Cursor extends TextureRect
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CONFINED_HIDDEN
+
+func _process(_delta: float) -> void:
+	position = get_global_mouse_position() - size/2
