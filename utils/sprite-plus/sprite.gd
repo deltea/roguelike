d2eec52d74e6ede737efc5d75e4cc7c2d8dc40e9
Leo Feng <thcheetah777@gmail.com>
2024-05-12 18:40:23 -0500

    🕶️ shadow!

--- utils/sprite-plus/sprite.gd
+++ utils/sprite-plus/sprite.gd
@@ -8,6 +8,12 @@ signal flash_finished
 @export var rotation_dynamics_enabled = true
 @export var rotation_dynamics: DynamicsResource
 
+@export_group("Drop Shadow")
+@export var shadow_enabled = false
+@export var shadow_color = Color.BLACK
+@export var shadow_offset = Vector2(4, 4)
+@export var shadow_z = -1
+
 @onready var flash_timer: Timer = $FlashTimer
 @onready var scale_dynamics_solver := Dynamics.create_dynamics_vector(scale_dynamics)
 @onready var rotation_dynamics_solver := Dynamics.create_dynamics(rotation_dynamics)
@@ -20,6 +26,13 @@ func _ready() -> void:
 	target_scale = scale
 	target_rotation_degrees = rotation_degrees
 
+	if shadow_enabled:
+		shadow = Sprite2D.new()
+		shadow.texture = texture
+		shadow.z_index = shadow_z
+		shadow.self_modulate = shadow_color
+		add_child(shadow)
+
 func _process(_delta: float) -> void:
 	if scale_dynamics and scale_dynamics_enabled:
 		scale = scale_dynamics_solver.update(target_scale)
@@ -30,6 +43,11 @@ func _process(_delta: float) -> void:
 		rotation_degrees = rotation_dynamics_solver.update(target_rotation_degrees)
 	else:
 		rotation_degrees = target_rotation_degrees
+
+	if shadow:
+		shadow.global_position = global_position + shadow_offset
+		shadow.global_rotation = global_rotation
+		shadow.global_scale = global_scale
 
 func impact_scale(value: Vector2):
 	scale_dynamics_solver.set_value(value)
