03132a8267c7d8aa3e364b7731941e5eefb20e5b
Leo Feng <thcheetah777@gmail.com>
2024-05-13 18:08:56 -0500

    3️⃣ 3d

--- assets/models/lock.gd
+++
@@ -1,4 +0,0 @@
-extends Node3D
-
-func _process(delta: float) -> void:
-	rotate_y(1.0 * delta)
--- rooms/level.gd
+++ rooms/level.gd
@@ -2,3 +2,4 @@ class_name Level extends Room
 
 func _ready() -> void:
 	$Model.texture = $SubViewport.get_texture()
+	$Model/Shadow.texture = $SubViewport.get_texture()
