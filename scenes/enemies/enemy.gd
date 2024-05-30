c7c76dd77cb9a583c53699d1c020788b0919322e
Leo Feng <thcheetah777@gmail.com>
2024-05-30 05:47:22 -0600

    🧃 enemy juice when dying

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -1,6 +1,6 @@
 class_name Enemy extends CharacterBody2D
 
-@export var max_health = 3000
+@export var max_health = 3
 
 @onready var sprite: Sprite = $Sprite
 @onready var collider: CollisionShape2D = $CollisionShape
@@ -15,15 +15,20 @@ func get_hurt(damage: int):
 	health -= damage
 
 	if health <= 0:
-		queue_free()
+		die()
 
 	sprite.impact_scale(Vector2.ONE * 1.3)
-	if RoomManager.current_room:
-		RoomManager.current_room.camera.shake(0.05, 1.5)
 
 	sprite.self_modulate = Color.BLUE
 	await Clock.wait(0.1)
 	sprite.self_modulate = Color.GREEN
+
+func die():
+	queue_free()
+
+	Clock.hitstop(0.05)
+	if RoomManager.current_room:
+		RoomManager.current_room.camera.shake(0.05, 3)
 
 func _on_hitbox_area_entered(area: Area2D):
 	if area is Bullet:
